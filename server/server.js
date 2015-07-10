5f73590a743299da881a15ad2a826b6d69a31ff2
bdstein33 <bdstein33@gmail.com>
2015-07-09 21:02:25 -0700

    (fix) Include body-parser and static file path in all environments.  Add conditional port environment.
    (fix) Include body-parser and static file path in all environments.  Add conditional port environment.
    
    (fix) Include body-parser and static file path in all environments.  Add conditional port environment.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 /* Sets the NODE_ENV variable equal to development if it is otherwise undefined.  In a production environment, NODE_ENV will be assigned a value from our host */
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.PORT = process.env.PORT || 3000;
 
 var express = require('./config/express');
 
 var app = express();
-app.listen(3000);
+app.listen(process.env.PORT);
 module.exports = app;
 
 console.log('Server running at http://localhost:3000/');
