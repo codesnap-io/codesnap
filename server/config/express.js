535dc4f5aa37fe0bc54f34af080a3c4c77c415cf
bdstein33 <bdstein33@gmail.com>
2015-07-09 21:02:25 -0700

    (fix) Include body-parser and static file path in all environments.  Add conditional port environment.
    (fix) Include body-parser and static file path in all environments.  Add conditional port environment.
    
    (fix) Include body-parser and static file path in all environments.  Add conditional port environment.

--- server/config/express.js
+++ server/config/express.js
@@ -12,18 +12,18 @@ module.exports = function() {
 
     /* morgan is middleware that logs server activity to the console.  We only want to use it in a development setting */
     app.use(morgan('dev'));
-
-
-    /* body-parser converts data receive in POST requests into JSON */
-    app.use(bodyParser.json());
-
-    /* Tell express where to look for static files.  The file listed becomes the root directory for static files. */
-    app.use(express.static('public'));
   }
+
+  /* body-parser converts data receive in POST requests into JSON */
+  app.use(bodyParser.json());
+
+  /* Tell express where to look for static files.  The file listed becomes the root directory for static files. */
+  app.use(express.static('public'));
 
 
   // Required Routes
   // require('../routes/users')
 
 return app;
+
 };
