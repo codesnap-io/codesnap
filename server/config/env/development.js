3ad68403fe0b2684f09b98d2046843f60511ae4f
m-arnold <michaelaarnold@gmail.com>
2015-07-10 16:06:11 -0700

    (feat) navbar works!

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,7 +1,7 @@
-(function(){
+(function () {
   'use strict';
 
-  process.env.PORT = 3000;
+  process.env.PORT = 8000;
   process.env.RDS_HOSTNAME = '127.0.0.1';
   process.env.RDS_USERNAME = 'root';
   process.env.RDS_PASSWORD = '';
