20c0882bf222b3a58bc1e61d765189b869d44723
bdstein33 <bdstein33@gmail.com>
2015-07-10 12:16:58 -0700

    (structure) Establish MySQL database connection.  Wrote user schema and create users table.

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,8 +1,15 @@
 (function(){
   'use strict';
 
+  process.env.PORT = 3000;
+  process.env.DB_HOST = '127.0.0.1';
+  process.env.DB_USERNAME = 'root';
+  process.env.DB_PASSWORD = '';
+  process.env.DB_NAME = 'crouton';
+
   module.exports = {
     // jwtSecret: process.env.JWT_SECRET || 'abc'
+
   };
 
 })();
--- server/models/user.server.model.js
+++ server/models/user.server.model.js
@@ -1,4 +1,30 @@
 (function(){
   'use strict';
 
+  var db = require('../config/db');
+
+  db.knex.schema.hasTable('users').then(function(exists) {
+    /* Drops the table if it exists.  This is useful to uncomment when you are working on editing the schema */
+    // if (exists) {
+    //   db.knex.schema.dropTable('users').then(function() {
+    //     console.log("Removed User Table");
+    //   });
+    // }
+
+
+    /* Create users table if it doesn't exist. */
+    if (!exists) {
+      db.knex.schema.createTable('users', function(user) {
+        user.increments('id').primary();
+        user.string('first_name', 30);
+        user.string('last_name', 30);
+        user.string('email', 30);
+        user.timestamps(); /* Creates created_at, updated_at */
+      }).then(function(table) {
+        console.log('Created Users Table');
+      });
+    }
+  });
+
+
 })();
