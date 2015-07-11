f7e1117f6869cc421ff55e691a22787aad932edb
bdstein33 <bdstein33@gmail.com>
2015-07-11 15:30:44 -0700

    (doc) Change user schema to just have name instead of first_name, last_name
    (doc) Uncomment drop user table so it always creates a new table when the server runs

--- server/models/user.server.model.js
+++ server/models/user.server.model.js
@@ -9,19 +9,18 @@
 
   db.knex.schema.hasTable('users').then(function(exists) {
     /* Drops the table if it exists.  This is useful to uncomment when you are working on editing the schema */
-    // if (exists) {
-    //   db.knex.schema.dropTable('users').then(function() {
-    //     console.log("Removed User Table");
-    //   });
-    // }
+    if (exists) {
+      db.knex.schema.dropTable('users').then(function() {
+        console.log("Removed User Table");
+      });
+    }
 
 
     /* Create users table if it doesn't exist. */
     if (!exists) {
       db.knex.schema.createTable('users', function(user) {
         user.increments('id').primary();
-        user.string('first_name', 30);
-        user.string('last_name', 30);
+        user.string('name', 60);
         user.string('email', 30);
         user.string('username', 30);
         user.integer('github_id');
