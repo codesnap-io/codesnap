bfbd76b917cbaa1116303a3e106ccc5c640b2149
bdstein33 <bdstein33@gmail.com>
2015-07-11 15:17:22 -0700

    (fix) Clean up some code.

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,21 +1,22 @@
 var GitHubStrategy = require('passport-github2').Strategy;
-var development = require('../config/env/development.js');
+// var development = require('../config/env/development.js');
 var passport = require("passport");
 var User = require('../models/user.server.model.js');
 
 exports.githubStrategy = function() {
   passport.use(new GitHubStrategy({
-      clientID: development.githubClientID,
-      clientSecret: development.githubClientSecret,
-      callbackURL: development.githubCallbackUrl
+      /* These variables are tied to our application's account.  By including them here, we gain access to the users we authenticate */
+      clientID: process.env.githubClientID,
+      clientSecret: process.env.githubClientSecret,
+      callbackURL: process.env.githubCallbackUrl
     },
     function(accessToken, refreshToken, profile, done) {
-      console.log(profile);
       User.where({
           github_id: profile.id
       })
       .fetch()
       .then(function(user) {
+        /* Check to see if user exists.  If user does not exist, create user */
         if (!user) {
           User.forge({
             github_id: profile.id,
@@ -29,20 +30,20 @@ exports.githubStrategy = function() {
           });
         }
     }).catch(function(err) {
-        console.log('error');
         console.log(err);
       });
   }));
 };
 
+/* Determines what data from the user object should be stored in the session result of serializeUser method is attached to the session as req.session.passport.user */
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
+/* Pass in key that is saved in req.session.passport.user -- this key is used to retrieve the user object attaches the user object to the request as req.user */
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
-
 
 
 /* this function can be used if we choose to run sessions */
