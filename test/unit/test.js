ceee747d3a2ac5fc0a576af71917c06ad9b10ad0
bdstein33 <bdstein33@gmail.com>
2015-07-10 19:15:34 -0700

    (merge) Fixed merge request conflict
    (merge) Fix another merge conflict

---
+++ test/unit/test.js
@@ -0,0 +1,11 @@
+// var assert = chai.assert;
+// var should = chai.should();
+// var expect = chai.expect();
+
+
+describe('An Example Suite', function() {
+
+  it('Should allow functional syntax for chai expectations', function() {
+    expect(true).to.be.true();
+  });
+});
