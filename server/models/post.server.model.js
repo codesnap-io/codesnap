8e6702ac7c0b8c1b62a3b95064c67d202d005969
bdstein33 <bdstein33@gmail.com>
2015-07-13 14:04:57 -0700

    (feat) Add add post POST request.  Add edit post POST request.

--- server/models/post.server.model.js
+++ server/models/post.server.model.js
@@ -3,6 +3,39 @@
   var db = require('../config/db');
   var Post = require('../config/schema').Post;
 
+  Post.add = function(postData, callback) {
+    new Post(postData)
+    .save()
+    .then(function(post) {
+      if (!post) {
+        callback("Invalid post data");
+      } else {
+        callback(null, post);
+      }
+    });
+  };
+
+  Post.edit = function(postData, callback){
+    new Post({'id': postData.id})
+    .fetch()
+    .then(function(post) {
+      /* If post doesn't exist, return error message */
+      if (!post) {
+        callback("Invalid post id");
+      } else {
+        /* If post exists, update all of its attributes with the data passed in from the POST request */
+        for (var key in postData) {
+          /* Check to make sure that the attribute we are trying to change already exists for the post.  If it doesn't, we know that it is an invalid column header and we don't want to add it.  */
+          if (post.get(key) !== undefined) {
+            post.set(key, postData[key]);
+          }
+        }
+        post.save();
+        callback(null, post.attributes);
+      }
+    });
+  };
+
   Post.postInfo = function(postId, callback) {
     /* Create a post object which we call
     .fetch() on to search the database to see
