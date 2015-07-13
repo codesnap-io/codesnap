28cd7570b8e7eb00c83c45aa300f173435859b52
m-arnold <michaelaarnold@gmail.com>
2015-07-13 14:57:21 -0700

    work on dependency injection

--- client/app/components/home/homeController.js
+++ client/app/components/home/homeController.js
@@ -2,23 +2,23 @@ angular.module('homeController', [])
 
 .controller('homeController', function ($scope) {
   $scope.posts = [{
-    id: 1,
-    title: "The Best Title Ever!",
-    authorPic: "https://d262ilb51hltx0.cloudfront.net/fit/c/80/80/0*xZl_kLRGSfBND02C.jpg",
-    author: "Michael Arnold",
-    content: "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content " +
-      "sample post content sample post content sample post content sample post content. "
-  }, {
+      id: 1,
+      title: "The Best Title Ever!",
+      authorPic: "https://d262ilb51hltx0.cloudfront.net/fit/c/80/80/0*xZl_kLRGSfBND02C.jpg",
+      author: "Michael Arnold",
+      content: "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content " +
+        "sample post content sample post content sample post content sample post content. "
+    }, {
     id: 2,
     title: "The Best Title Ever!",
     authorPic: "https://d262ilb51hltx0.cloudfront.net/fit/c/80/80/0*xZl_kLRGSfBND02C.jpg",
