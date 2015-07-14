9690dcca7da9c2648ea6503d7d2d62947af7ca42
bdstein33 <bdstein33@gmail.com>
2015-07-13 19:02:10 -0700

    (test) Remove coverage folder from client folder
    (merge) Fix merges

--- server/controllers/posts.server.controller.js
+++ server/controllers/posts.server.controller.js
@@ -25,7 +25,6 @@
         res.json(post);
       }
     });
-
   };
 
   exports.editPost = function(req, res) {
--- server/routes/users.server.routes.js
+++ server/routes/users.server.routes.js
@@ -8,25 +8,25 @@
   module.exports = function(app) {
     auth.githubStrategy();
 
-    // GET /auth/github
-    //   Use passport.authenticate() as route middleware to authenticate the
-    //   request.  The first step in GitHub authentication will involve redirecting
-    //   the user to github.com.  After authorization, GitHubwill redirect the user
-    //   back to this application at /auth/github/callback
+    /* GET /auth/github
+      Use passport.authenticate() as route middleware to authenticate the
+      request.  The first step in GitHub authentication will involve redirecting
+      the user to github.com.  After authorization, GitHubwill redirect the user
+      back to this application at /auth/github/callback */
 
     app.get('/auth/github', app.cors(),
       passport.authenticate('github', { scope: [ 'user', 'public_repo' ] }));
 
 
-    // GET /auth/github/callback
-    //   Use passport.authenticate() as route middleware to authenticate the
-    //   request.  If authentication fails, the user will be redirected back to the
-    //   login page.  Otherwise, the primary route function function will be called,
-    //   which, in this example, will redirect the user to the home page.
+    /* GET /auth/github/callback
+      Use passport.authenticate() as route middleware to authenticate the
+      request.  If authentication fails, the user will be redirected back to the
+      login page.  Otherwise, the primary route function function will be called,
+      which, in this example, will redirect the user to the home page. */
     app.get('/auth/github/callback', app.cors(),
       passport.authenticate('github', { failureRedirect: '/login' }),
       function(req, res) {
-        console.log("AAAAAAAAA");
+        
         res.redirect('/');
     });
 
