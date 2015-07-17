9940f91987e25ba0879f6c230685e25ddd917d2c
Chris <claymanchris@gmail.com>
2015-07-16 19:37:17 -0700

    [bug] remove navbar from signup

--- client/app/components/home/homeController.js
+++ client/app/components/home/homeController.js
@@ -2,6 +2,7 @@ angular.module('homeController', [])
 
 .controller('homeController', function ($scope, postFactory) {
 
+  /* show all posts */
   postFactory.getPostsData()
     .then(function (posts, err) {
       if (err) {
@@ -11,18 +12,19 @@ angular.module('homeController', [])
       }
     });
 
+
+  /* determines current topic, active tabs, and active filter in home view */
   $scope.chooseTopic = function (topic) {
     $scope.topic = topic;
   };
-
   $scope.isSelectedTopic = function (topic) {
     return topic === $scope.topic;
   };
-
   $scope.chooseFilter = function (filter) {
     $scope.topicFiler = filter;
   };
 
+  /* dummy shit */
   $scope.topicFilter = 'latest';
   $scope.topic = 'Ruby';
   $scope.topics = ['Ruby', 'Javascript', 'Haskell', 'Rust', 'Python',
--- client/app/components/user/userController.js
+++ client/app/components/user/userController.js
@@ -3,11 +3,14 @@ angular.module('userController', ['userFactory'])
 .controller('userController', function ($scope, userFactory) {
   $scope.userId = localStorage.userId;
 
+  /* passes in local user id to determine which profile to look at */
   userFactory.getUser($scope.userId)
     .then(function (user) {
       $scope.user = user;
     });
 
+
+  /* removes user from DB */
   $scope.removeUser = function () {
     userFactory.removeUser($scope.user.id)
       .then(function (user) {
