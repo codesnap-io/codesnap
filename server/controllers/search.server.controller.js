d0f9a0babc2a56130d043ff9daccf9f53640b0ce
m-arnold <michaelaarnold@gmail.com>
2015-07-20 12:26:01 -0700

    (feat) grab all titles, authors, and tags for front-end search

--- server/controllers/search.server.controller.js
+++ server/controllers/search.server.controller.js
@@ -1,6 +1,8 @@
-(function () {
+(function() {
   'use strict';
   var Fuse = require('../services/fuse.js');
+  var search = require('../models/search.server.model');
+  var tag = require('../models/tag.server.model');
 
   var options = {
     caseSensitive: false,
@@ -14,19 +16,59 @@
     keys: ["name"]
   };
 
-  var list = [
-    {"name": "Angular"},
-    {"name": "angular.js"},
-    {"name": "angularjs"},
-    {"name": "bngularjs"},
-    {"name": "bsgular"},
-    {"name": "Angulr"},
-    {"name": "Angular.j"},
-    {"name": "Angular"}
-  ];
+  var list = [{
+    "name": "Angular"
+  }, {
+    "name": "angular.js"
+  }, {
+    "name": "angularjs"
+  }, {
+    "name": "bngularjs"
+  }, {
+    "name": "bsgular"
+  }, {
+    "name": "Angulr"
+  }, {
+    "name": "Angular.j"
+  }, {
+    "name": "Angular"
+  }];
 
   var fuse = new Fuse(list, options); // "list" is the item array
   var result = fuse.search("angular");
   console.log(result);
+
+  //return all metadata in form {titles: [], authors: [], tags: []}
+  exports.getAllMetadata = function(req, res) {
+    console.log("search: ", search);
+    var metadata = {};
+    //grab all titles from db
+    search.getAllTitles(function(titles) {
+        metadata.titles = titles;
+        //grab authors
+        search.getAllAuthors(function(authors) {
+          metadata.authors = authors;
+          //grab tags
+          tag.getAll(function(tags) {
+            metadata.tags = tags;
+            //return all
+            console.log("sending back metadata: ", metadata);
+            res.json(metadata);
+          })
+        })
+      })
+      //grab all authors from db
+      // search.getAllAuthors(function(authors, error) {
+      //   if (error) {
+      //     console.log("error getting authors: ", error);
+      //   } else {
+      //     metadata.authors = authors;
+      //   }
+      // });
+      // //grab all tags from db
+      // tag.getAll(function(tags) {
+      //   metadata.tags = tags;
+      // })
+  };
 
 })();
