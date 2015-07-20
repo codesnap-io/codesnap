dff1814085e30a9482d41f716f9a0d74547083cd
m-arnold <michaelaarnold@gmail.com>
2015-07-20 16:13:18 -0700

    minor fixes and button to pull posts without refresh

--- server/controllers/search.server.controller.js
+++ server/controllers/search.server.controller.js
@@ -1,18 +1,17 @@
 (function() {
   'use strict';
-  var search = require('../models/search.server.model');
   var tag = require('../models/tag.server.model');
+  var Post = require('../config/schema').Post;
   var Promise = require('bluebird');
 
   //return all metadata in form {titles: [], authors: [], tags: []}
   exports.getAllMetadata = function(req, res) {
     var metadata = {};
     //grab all titles from db
-    //Promise.join allows us to wait for several concurrent promises to finish
-    //before firing "then"
+    //Promise.join allows us to wait for several concurrent promises to finish before firing "then"
     Promise.join(
-        search.getAllTitles(),
-        search.getAllAuthors(),
+        Post.getAllTitles(),
+        Post.getAllAuthors(),
         tag.getAllPromise(),
         function(titleData, authorData, tagData) {
           metadata.titles = titleData.map(function(title) {
--- server/models/post.server.model.js
+++ server/models/post.server.model.js
@@ -1,12 +1,12 @@
-(function () {
+(function() {
   'use strict';
   var db = require('../config/db');
   var Post = require('../config/schema').Post;
 
-  Post.add = function (postData, callback) {
+  Post.add = function(postData, callback) {
     new Post(postData)
       .save()
-      .then(function (post) {
+      .then(function(post) {
         if (!post) {
           callback("Invalid post data");
         } else {
@@ -20,30 +20,30 @@
      .fetch() on to search the database to see
      if that post already exists */
     new Post({
-      'url': postUrl
-    })
-        .fetch()
+        'url': postUrl
+      })
+      .fetch()
       /* .fetch() returns a promise so we call .then() */
-        .then(function (post) {
-          /* If the user doesn't exist, return error message. Otherwise return profile information */
-          if (!post) {
-            callback("Invalid post url.\n");
-          } else {
-            db.knex('posts').where('url', postUrl)
-                .del()
-                .then(function () {
-                  callback(null, post);
-                });
-          }
-        });
+      .then(function(post) {
+        /* If the user doesn't exist, return error message. Otherwise return profile information */
+        if (!post) {
+          callback("Invalid post url.\n");
+        } else {
+          db.knex('posts').where('url', postUrl)
+            .del()
+            .then(function() {
+              callback(null, post);
+            });
+        }
+      });
   };
 
-  Post.modify = function (postData, callback) {
+  Post.modify = function(postData, callback) {
     new Post({
-      'url': postData.url
-    })
+        'url': postData.url
+      })
       .fetch()
-      .then(function (post) {
+      .then(function(post) {
         /* If post doesn't exist, return error message */
         if (!post) {
           callback("Invalid post url");
@@ -61,21 +61,21 @@
       });
   };
 
-  Post.postInfo = function (postId, callback) {
+  Post.postInfo = function(postId, callback) {
     /* Create a post object which we call
     .fetch() on to search the database to see
     if that post already exists */
     new Post({
-      'id': postId
-    })
+        'id': postId
+      })
       .fetch()
-    /* .fetch() returns a promise so we call .then() */
-    .then(function (post) {
-      /* If the post doesn't exist, return error message. Otherwise return profile information */
-      if (!post) {
-        callback("Invalid post id.\n");
-      } else {
-        db.knex.raw(' \
+      /* .fetch() returns a promise so we call .then() */
+      .then(function(post) {
+        /* If the post doesn't exist, return error message. Otherwise return profile information */
+        if (!post) {
+          callback("Invalid post id.\n");
+        } else {
+          db.knex.raw(' \
           SELECT \
             posts.id AS post_id, \
             posts.title AS post_title, \
@@ -87,10 +87,10 @@
           FROM posts, users \
           WHERE posts.user_id = users.id \
             AND posts.id = ' + postId)
-        .then(function (data) {
-          var postData = data[0][0];
+            .then(function(data) {
+              var postData = data[0][0];
 
-          db.knex.raw(' \
+              db.knex.raw(' \
             SELECT \
             tags.title AS title, \
             tags.id AS id \
@@ -98,26 +98,26 @@
             WHERE posts.id = post_tag_join.post_id \
               AND post_tag_join.tag_id = tags.id \
               AND posts.id = ' + postId)
-          .then(function(data) {
-            postData.tags = data[0];
-            callback(null, postData);
+                .then(function(data) {
+                  postData.tags = data[0];
+                  callback(null, postData);
 
-          });
-        });
-      }
-    });
+                });
+            });
+        }
+      });
   };
 
-  Post.getPostsOnQuery = function (query, queryType, callback) {
+  Post.getPostsOnQuery = function(query, queryType, callback) {
     db.knex.select('posts.id AS post_id', 'posts.title AS post_title',
-    'posts.url AS post_url', 'users.name AS author', 'users.profile_photo_url AS profile_photo_url')
-    .from('posts').where(queryType, 'like', '%' + query + '%').leftOuterJoin('users', 'posts.user_id', 'users.id')
-    .then(function(data) {
-      callback(null, data);
-    });
+        'posts.url AS post_url', 'users.name AS author', 'users.profile_photo_url AS profile_photo_url')
+      .from('posts').where(queryType, 'like', '%' + query + '%').leftOuterJoin('users', 'posts.user_id', 'users.id')
+      .then(function(data) {
+        callback(null, data);
+      });
   };
 
-  Post.getAllPosts = function (callback, options) {
+  Post.getAllPosts = function(callback, options) {
     db.knex.raw(' \
       SELECT \
         posts.id AS post_id, \
@@ -127,9 +127,20 @@
         users.profile_photo_url AS profile_photo_url \
       FROM posts, users \
       WHERE posts.user_id = users.id')
-    .then(function (data) {
-      callback(null, data[0]);
-    });
+      .then(function(data) {
+        callback(null, data[0]);
+      });
+  };
+
+  Post.getAllTitles = function() {
+    return db.knex
+      .select('title').from('posts');
+  };
+
+  Post.getAllAuthors = function() {
+    return db.knex
+      .distinct()
+      .select('username').from('users');
   };
 
   module.exports = Post;
