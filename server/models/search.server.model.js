9a636e2ef403d26ef64b75d5b7551169e704cc1f
m-arnold <michaelaarnold@gmail.com>
2015-07-20 14:25:57 -0700

    (feat) refactor /search/all to use promises

--- server/models/search.server.model.js
+++ server/models/search.server.model.js
@@ -3,29 +3,29 @@
 
   var db = require('../config/db');
 
-  exports.getAllTitles = function(callback) {
-    db.knex
-      .select('title').from('posts')
-      .then(function(data) {
-        var titles = data.map(function(title) {
-          return title.title;
-        });
-        console.log("got titles: ", titles);
-        return callback(titles);
-      });
+  exports.getAllTitles = function() {
+    return db.knex
+      .select('title').from('posts');
+    // .then(function(data) {
+    //   var titles = data.map(function(title) {
+    //     return title.title;
+    //   });
+    //   console.log("got titles: ", titles);
+    //   return callback(titles);
+    // });
   };
 
-  exports.getAllAuthors = function(callback) {
-    db.knex
+  exports.getAllAuthors = function() {
+    return db.knex
       .distinct()
-      .select('username').from('users')
-      .then(function(data) {
-        var authors = data.map(function(author) {
-          return author.username;
-        });
-        console.log("got authors: ", authors);
-        return callback(authors);
-      });
+      .select('username').from('users');
+    // .then(function(data) {
+    //   var authors = data.map(function(author) {
+    //     return author.username;
+    //   });
+    //   console.log("got authors: ", authors);
+    //   return callback(authors);
+    // });
   };
 
 })();
--- server/models/tag.server.model.js
+++ server/models/tag.server.model.js
@@ -1,34 +1,39 @@
-(function(){
+(function() {
   'use strict';
   var db = require('../config/db');
   var Tag = require('../config/schema').Tag;
 
   Tag.getAll = function(callback) {
     db.knex.raw(' SELECT title AS tag_title FROM tags')
-    .then(function(data, error) {
-      console.log(data[0]);
-      callback(error, data[0]);
-    });
+      .then(function(data, error) {
+        console.log(data[0]);
+        callback(error, data[0]);
+      });
+  };
+
+  Tag.getAllPromise = function() {
+    return db.knex.select('title').from('tags');
   };
 
   Tag.createOrSave = function(tagTitle, callback) {
-    new Tag({'title': tagTitle})
-    .fetch()
-    .then(function(tag) {
-      if (!tag) {
-        new Tag({'title': tagTitle})
-        .save()
-        .then(function(tag) {
-          callback(null, tag);
-        });
-      } else {
-        callback("Already exists");
-      }
-    });
+    new Tag({
+        'title': tagTitle
+      })
+      .fetch()
+      .then(function(tag) {
+        if (!tag) {
+          new Tag({
+              'title': tagTitle
+            })
+            .save()
+            .then(function(tag) {
+              callback(null, tag);
+            });
+        } else {
+          callback("Already exists");
+        }
+      });
   };
 
   module.exports = Tag;
 })();
-
-
-
