2dc62dd02aa62d3640f00cd03d59301bc2e1531c
m-arnold <michaelaarnold@gmail.com>
2015-07-22 13:21:55 -0700

    style account page
    store user on rootScope
    
    rename "profile" to "account"

--- client/app/shared/user/userController.js
+++ client/app/shared/user/userController.js
@@ -1,18 +1,17 @@
 angular.module('userController', ['userFactory'])
 
-.controller('userController', function($scope, $state, userFactory) {
+.controller('userController', function($rootScope, $scope, $state, userFactory) {
   /* passes in local user id to determine which profile to look at */
   // userFactory.getUser()
   //   .then(function(user) {
   //     $scope.user = user;
   //   });
-  $scope.user = userFactory.getCurrentUser();
-
 
   /* removes user from DB */
   $scope.removeUser = function() {
     userFactory.removeUser();
     delete window.localStorage.jwtToken;
+    delete $rootScope.user;
     $state.go('signup');
   };
 
--- client/app/shared/user/userFactory.js
+++ client/app/shared/user/userFactory.js
@@ -3,7 +3,6 @@
   angular.module('userFactory', [])
 
   .factory('userFactory', function($http, $state, authFactory) {
-    var currentUser = {};
     return {
       /* Get user information from server including:
          name, username, id, profile_photo_url, posts */
@@ -27,7 +26,6 @@
 
       /* Send user id to server to remove user and associated posts from database */
       removeUser: function() {
-        currentUser = {};
         return $http({
           method: 'DELETE',
           url: '/user/info/',
@@ -35,15 +33,6 @@
             user_id: localStorage.jwtToken
           }
         });
-      },
-
-      /*Sets current user data for passing between user controller and navs / subnavs */
-      /* currently initially set in nav controller */
-      setCurrentUser: function(userObj) {
-        currentUser = userObj;
-      },
-      getCurrentUser: function() {
-        return currentUser;
       }
 
     };
