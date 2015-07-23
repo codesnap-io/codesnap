adf90296a30a673dfdf99765540418a539b0501b
bdstein33 <bdstein33@gmail.com>
2015-07-22 18:42:33 -0700

    (feat) Add heart toggle functionality to post page

--- client/app/shared/subnavs/postSubnavDirective.js
+++ client/app/shared/subnavs/postSubnavDirective.js
@@ -4,7 +4,7 @@
   .directive('postSubnav', function(postFactory, userFactory) {
       return {
         restrict: 'A',
-        controller: function ($scope, postFactory, userFactory, $timeout) {
+        controller: function ($scope, $rootScope, postFactory, userFactory, $timeout) {
           // $scope.user = userFactory.getCurrentUser();
 
           // loads post data retrieved by post controller. timeout provides time for async
@@ -17,30 +17,43 @@
                 });
           }, 200);
 
+          $scope.loggedIn = $rootScope.loggedIn;
 
           // Toggle likes
           $scope.toggleLike = function() {
-            postFactory.toggleLike(window.localStorage.jwtToken, $scope.postData.post_id)
-              .then(function(resp) {
-                console.log(resp);
-              });
+            if ($scope.loggedIn) {
+              postFactory.toggleLike(localStorage.jwtToken, $scope.postData.post_id)
+                .then(function(resp) {
 
-            //client side updates
-            if ($scope.isLiked) {
-              $scope.postData.likes--;
+                  /* Update like count in client */
+                  if ($scope.isLiked) {
+                    $scope.postData.likes--;
+                  } else {
+                    $scope.postData.likes++;
+                  }
+
+                  $scope.isLiked = !$scope.isLiked;
+                });
             } else {
-              $scope.postData.likes++;
+              /* If user tries to like post without being logged in, show a pop up telling them that they need to log in */
+              $('#post-error-container').slideDown(300);
             }
-
-            $scope.isLiked = !$scope.isLiked;
           };
 
+          if($scope.loggedIn) {
+            console.log("A");
+            $scope.isLiked = function() {
+              return $scope.isLiked;
+            };
+          }
 
-          $scope.isLiked = function() {
-            return $scope.isLiked;
-          };
+          /* Hide post error container when user clicks it */
 
-
+        },
+        link: function(scope, elem, attr) {
+          $('#post-error-container').click(function() {
+            $(this).hide();
+          });
         }
       };
     });
