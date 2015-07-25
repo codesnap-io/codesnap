8cf2a3c4187ad14af0b88890450ab3c5ad4ae7cc
Chris <claymanchris@gmail.com>
2015-07-25 14:53:33 -0700

    [build] configuration for staging, quick fix to list posts in tag view

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,10 @@
     module.exports = require('./env/production.js');
   }
 
+  if (process.env.NODE_ENV === 'staging') {
+    module.exports = require('./env/staging.js');
+  }
+
   /* Injects environment logic into express.js and mongoose.js depending on whether you are working in development or production environment */
   if (process.env.NODE_ENV !== 'production') {
     module.exports = require('./env/development.js');
