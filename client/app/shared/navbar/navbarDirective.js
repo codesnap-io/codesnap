4d4ac89199bde36b6b47f13406c649d4ed832025
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-07-24 18:37:12 -0700

    Merging changes

--- client/app/shared/navbar/navbarDirective.js
+++ client/app/shared/navbar/navbarDirective.js
@@ -31,11 +31,20 @@
 
           if (!$rootScope.user && !!token) {
             userFactory.getUser()
+              .then(function(resp) {
+                //address issue where database dropped while logged in, causing token to be out of date.
+                if (resp.data === "Invalid user id.\n") {
+                  console.log("user log in weirdness detected. Deleting token and redirecting to /signup.");
+                  delete window.localStorage.codeSnapJwtToken;
+                  $state.go('signup');
+                }
+                return resp.data;
+              })
               .then(function(user) {
-                $rootScope.user = user;
-                $scope.loggedIn = !!user;
-                $scope.newPostUrl = "/post/add?username=" + user.username;
-              });
+              $rootScope.user = user;
+              $scope.loggedIn = !!user;
+              $scope.newPostUrl = "/post/add?username=" + user.username;
+            });
           }
         });
       },
