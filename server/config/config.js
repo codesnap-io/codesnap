27deb86b2f69eac8512bc8ba93b0204fe1029749
Chris <claymanchris@gmail.com>
2015-07-27 12:19:39 -0700

    [build] configuration fix

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@
   }
 
   /* Injects environment logic into express.js and mongoose.js depending on whether you are working in development or production environment */
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV =  == 'development') {
     module.exports = require('./env/development.js');
   }
 
