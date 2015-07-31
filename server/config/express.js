e8659741427e6e3875a7e6d9f7dff40fef1e6618
Chris <claymanchris@gmail.com>
2015-07-31 11:10:45 -0700

    [cleanup] removed cors. fixed exposed secret. deleted more dependencies

--- server/config/express.js
+++ server/config/express.js
@@ -7,31 +7,17 @@
   var bodyParser = require('body-parser');
   var passport = require('passport');
   var path = require('path');
-  var cors = require('cors');
   var session = require('express-session');
 
   module.exports = function () {
     var app = express();
-    app.use(session({ secret: 'qoiwej2o3iej23', cookie: { maxAge: 60000 }}));
+    app.use(session({ secret: process.env.SESSION_SECRET, cookie: { maxAge: 60000 }}));
 
     if (process.env.NODE_ENV === 'development') {
       /* morgan is middleware that logs server activity to the console.  We only want to use it in a development setting */
       app.use(morgan('dev'));
     }
 
-    /* cors initialization and options for whitelist */
-    app.cors = cors;
-
-
-    /* whitelist of CORS available origins */
-    // TODO: change these origins for production
-    var whitelist = ['http://www.codesnap.io'];
-    app.corsOptions = {
-      origin: function(origin, callback){
-        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-        callback(null, originIsWhitelisted);
-      }
-    };
 
     /* body-parser converts data receive in POST requests into JSON */
     app.use(bodyParser.json());
