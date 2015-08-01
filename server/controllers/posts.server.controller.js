6f35b29f842761f0029099083150fdaf1ef5ff91
bdstein33 <bdstein33@gmail.com>
2015-08-01 11:25:55 -0700

    (fix) Commented out some code

--- server/controllers/posts.server.controller.js
+++ server/controllers/posts.server.controller.js
@@ -420,27 +420,27 @@
   };
 
   /* Dummy Data */
-  if (process.env.NODE_ENV === 'development') {
-    var req = {};
-    var res = {
-      sendStatus: function() {
-        return;
-      }
-    };
-    req.body = {
-      repository: {
-        name: 'codesnap.io',
-        owner: {
-          name: 'bdstein33'
-        }
-      },
-      head_commit: {
-        added: [],
-        removed: [],
-        modified: ['posts/myFirstPost.md']
-      }
-    };
-    exports.postReceive(req, res);
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   var req = {};
+  //   var res = {
+  //     sendStatus: function() {
+  //       return;
+  //     }
+  //   };
+  //   req.body = {
+  //     repository: {
+  //       name: 'codesnap.io',
+  //       owner: {
+  //         name: 'bdstein33'
+  //       }
+  //     },
+  //     head_commit: {
+  //       added: [],
+  //       removed: [],
+  //       modified: ['posts/myFirstPost.md']
+  //     }
+  //   };
+  //   exports.postReceive(req, res);
+  // }
 
 })();
