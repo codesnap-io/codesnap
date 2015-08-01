05cc6065fcf9bc2cd189764c7df15e47de0315a0
m-arnold <michaelaarnold@gmail.com>
2015-08-01 10:16:37 -0700

    fix gitGHUSer auth

--- server/services/repo.server.service.js
+++ server/services/repo.server.service.js
@@ -86,9 +86,12 @@
       url: "https://api.github.com/users/" + username,
       method: 'GET',
       headers: {
-        'Authorization': 'token ' + process.env.MICHAELTOKEN,
         'Accept': 'application/vnd.github.v3+json',
         'User-Agent': 'CodeSnap'
+      },
+      params: {
+        'client_id': '7d292489ff2489c0dc96',
+        'client_secret': 'd3ca1aa8a19339272e0425026b581e2e6294e2f9'
       }
     };
     return rp(options);
